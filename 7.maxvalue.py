7c9e9a97aaed21895fb5add334f961e0fbf47ace
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-08-27 16:09:29 +0530

    Day-6

---
+++ 7.maxvalue.py
@@ -0,0 +1,22 @@
+# Python program to find the Largest element of the array 
+
+# function to find the Largest element of array
+def FindLargestElement(arr,n):
+	maxVal = arr[0]
+
+	for i in range(1, n):
+		if arr[i] > maxVal:
+			maxVal = arr[i]
+	return maxVal
+
+# code to take user input for the array...
+n = int(input("Enter the number of elements in the array: "))
+arr = []
+print("Enter elements of the array: ")
+for i in range(n):
+  numbers = int(input())
+  arr.append(numbers)
+  
+# Calling the method to find the largest value  
+maxVal = FindLargestElement(arr,n)
+print ("Largest in given array is",maxVal)
