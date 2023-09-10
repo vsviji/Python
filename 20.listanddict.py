b091a48c997fd7d58404812c6bbb52262d0b8856
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-09-10 11:44:42 +0530

    Day-19

---
+++ 20.listanddict.py
@@ -0,0 +1,9 @@
+#list comprehension
+list_comp = [i for i in range(4)]  
+print(list_comp)  
+#op [0, 1, 2, 3]
+
+#dict comprehension
+dictt = {i : i+2 for i in range(10)}  
+print(dictt)  
+#op {0: 2, 1: 3, 2: 4, 3: 5, 4: 6, 5: 7, 6: 8,7: 9,8: 10,9: 11}
