4e7d0ef59e64ae4fc2817c05c97ff44ced97fa2e
Vijayalakshmi <127862956+vsviji@users.noreply.github.com>
2023-10-08 20:51:16 +0530

    Delete 20.revnumber.py

--- 20.revnumber.py
+++
@@ -1,9 +0,0 @@
-n= int(input("Enter number: "))  
-  
-reverse = 0  
-  
-while(n>0):  
-    digit = n%10  
-    reverse = reverse*10+digit  
-    n=n//10  
-print("The reverse of the number:",reverse)  
